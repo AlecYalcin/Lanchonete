bc113f6345653cec5bcc29b80fd70de57ad14291
Alec Yalcin <alec.yalcin@escolar.ifrn.edu.br>
2022-12-19 18:23:16 -0300

    Alterado o Banco de Dados para MYSQL'

--- Lanchonete/settings.py
+++ Lanchonete/settings.py
@@ -64,11 +64,14 @@ WSGI_APPLICATION = 'Lanchonete.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_20191104010011',
+        'USER': 'user_20191104010011',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
